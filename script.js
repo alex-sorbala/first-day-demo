31474a0bf47d73edae1f9b8043fa11eee2db83fe
Alexandru S <alexandru11723@gmail.com>
2023-04-06 03:11:48 -0600

    test 2

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-fetch("http://oldfunshinyjoke.neverssl.com/online/")
+fetch("http://oldfunshinyjoke.neverssl.com/online/", 
+{ 
+    mode: 'no-cors',
+    referrer: "",
+    referrerPolicy: "no-referrer",
+    redirect: "follow"
+}
+)
 .then((response)=>{
-    return response;
+    return response.text();
 })
 .then((data)=>{
     console.log(data);
