dd4a02bf2fea8695751871e293c3606fe2218fc0
Alexandru S <alexandru11723@gmail.com>
2024-02-13 14:08:46 -0700

    buttons for both options

--- script.js
+++ script.js
@@ -2,11 +2,29 @@
 var ExtensionId = "jnkehcgpmjanaaigkjmgegnkfgomhhlg";
 
 
-console.log("test");
-chrome.runtime.sendMessage(ExtensionId, {greeting: "hello"}, (response) => {
+
+
+function direct(){
+  console.log("Sending directly from browser");
+  fetch("http://10.0.0.43:6200", {
+          method: 'POST',
+          body: "<DETAIL><TRAN_TYPE>CCR1<TRAN_TYPE></DETAIL>"
+        })
+        .then(response => response.text())
+        .then(data => {
+          // do something with the data
+          console.log("direct response " + data);
+        })
+        .catch(error => {
+          // handle the error
+        });
+}
+
+function viaExtension(){
+  console.log("Sending to extension");
+  chrome.runtime.sendMessage(ExtensionId, {greeting: "hello"}, (response) => {
   // 3. Got an asynchronous response with the data from the service worker
-  console.log('received user data', response);
-});
-
-
+    console.log('response from extension ', response);
+  });
+}
   
